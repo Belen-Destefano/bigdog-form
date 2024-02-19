9cb163431eb14f3c537d7d535385c2da65f04c18
Belen Destefano <belen.destefano@gmail.com>
2024-02-19 15:45:03 -0300

    Adding inline styles to the form

--- src/App.jsx
+++ src/App.jsx
@@ -54,87 +54,101 @@ function App() {
     
     console.log('Tamaño:', selectedRadio);
 
+    const secretResponse = isDeveloper & selectedRadio === 'Big' ? '¡Contraseña secreta ingresada con éxito! Un agente secreto de Big Dog está en camino para comunicarse con usted. Por razones de seguridad, le pedimos que cierre esta página web y espere instrucciones. ¡Gracias por confiar en Big Dog para resolver su Web!' : 'Formulario enviado! Comenzaremos a ayudarlo con la busqueda de un perro con las descripciones establecidas. Esperamos encontrarle el mejor amigo fiel que pueda imaginar y supere sus expectativas'
+
+    alert(secretResponse)
 
 
   }
 
   return (
-    <div>
-      <h2>Formulario</h2>
-      <form onSubmit={handleFormSubmit}>
-       {/* Nombre */}
-        <div>
-          <label htmlFor="nombre">Nombre del Perro</label><br />
-          <input type="text" id="nombre" name="nombre" onChange={handleInputChange} required/>
-        </div>
+    <div style={{backdropFilter: 'blur(28px)', padding: '2%',  boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)', width: '100%'}}>
+      <h1>Formulario de Adopcion Canina</h1>
+      <h2>Como quisieras que sea tu amigo perruno ideal </h2>
+      <div style={   { display: 'flex', flexDirection: 'row'} }>
 
-        <br />
-       {/* Personalidad */}
-        <div>
-          <label>
-            Habilidades Caninas
-            {checkboxValues.map((checkbox, index) => (
-              <div key={index}>
-                <input
-                  type="checkbox"
-                  name={checkbox.name}
-                  checked={checkbox.checked}
-                  onChange={handleCheckboxChange}
-                />
-            {checkbox.name}
-              </div>
-            ))}
-          </label>
-        </div>
-        
-        <br />
-       {/* Raza  */}
-        <div>
-          <label>
-            Raza 
-            <br />
-            <select value={selectedOption} onChange={handleSelectChange}>
-              <option value="">Selecciona una opción</option>
-              <option value="Pura">Raza Pura</option>
-              <option value="Mezcla">Mezcla de Raza</option>
-              <option value="Developer">Mezcla de JS y React</option>
-              <option value="Mestizo">Mestizo</option>
-            </select>
-          </label>
-        </div>
+        <form onSubmit={handleFormSubmit} style={{width: '40%'}}>
+        {/* Nombre */}
+          <div>
+            <label htmlFor="nombre">Nombre del Perro</label><br />
+            <input type="text" id="nombre" name="nombre" onChange={handleInputChange} required/>
+          </div>
 
-        <br />
-        {/* Tamaño */}
-        <div>
-          <label>Tamaño</label>
+          <br />
+        {/* Personalidad */}
           <div>
             <label>
-              <input
-                type="radio"
-                value="Big"
-                checked={selectedRadio === 'Big'}
-                onChange={handleRadioChange}
-              />
-            Big Dog
+              Habilidades Caninas
+              {checkboxValues.map((checkbox, index) => (
+                <div key={index}>
+                  <input
+                    type="checkbox"
+                    name={checkbox.name}
+                    checked={checkbox.checked}
+                    onChange={handleCheckboxChange}
+                  />
+              {checkbox.name}
+                </div>
+              ))}
             </label>
           </div>
-   
+          
+          <br />
+        {/* Raza  */}
           <div>
             <label>
-              <input
-                type="radio"
-                value="Little"
-                checked={selectedRadio === 'Little'}
-                onChange={handleRadioChange}
-              />
-            Little Dog
+              Raza 
+              <br />
+              <select value={selectedOption} onChange={handleSelectChange}>
+                <option value="">Selecciona una opción</option>
+                <option value="Pura">Raza Pura</option>
+                <option value="Mezcla">Mezcla de Raza</option>
+                <option value="Developer">Mezcla de JS y React</option>
+                <option value="Mestizo">Mestizo</option>
+              </select>
             </label>
-          </div> 
-        </div> 
+          </div>
 
-        {/* Boton Submit */}
-      <button type="submit">Enviar</button>
-      </form>
+          <br />
+          {/* Tamaño */}
+          <div>
+            <label>Tamaño</label>
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  value="Big"
+                  checked={selectedRadio === 'Big'}
+                  onChange={handleRadioChange}
+                />
+              Big Dog
+              </label>
+            </div>
+    
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  value="Little"
+                  checked={selectedRadio === 'Little'}
+                  onChange={handleRadioChange}
+                />
+              Little Dog
+              </label>
+            </div> 
+          </div> 
+          
+          <br />
+          {/* Boton Submit */}
+        <button type="submit">Enviar</button>
+        </form>
+
+        <div style={{ width: '100%', backgroundImage: 'url("/backgroundDog.png")', backgroundSize: 'cover' }}>
+         {/* blabla */}
+      </div>
+
+
+      </div>
     </div>
   );
 }
